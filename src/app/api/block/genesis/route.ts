812e9ec81633442f571331d0d332e95eaf77a84c
pr-citrate <pr.citrate@gmail.com>
2024-08-27 22:18:39 +0900

    Fix: remove version of genesis block

--- src/app/api/block/genesis/route.ts
+++ src/app/api/block/genesis/route.ts
@@ -1,7 +1,6 @@
 import {NextResponse} from "next/server";
 import {Block, PrismaClient} from "@prisma/client";
 import merkle from "merkle";
-import {getVersion} from "@/utils/blockchain";
 
 const prisma: PrismaClient = new PrismaClient();
 
@@ -9,7 +8,6 @@ export async function POST(): Promise<NextResponse> {
     const tree = merkle('sha256').sync([]);
     const result: Block = await prisma.block.create({
         data: {
-            version: getVersion(),
             previousHash: '0'.repeat(64),
             timestamp: new Date(),
             body: 'Genesis Block',
